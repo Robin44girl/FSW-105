ae2403ece174187cbece0b9ddac7d6f91253b06f
Robin Johnson <57020290+Robin44girl@users.noreply.github.com>
2020-03-19 20:12:32 -0400

    if_statements_olympics

---
+++ week2/ifstatements.js
@@ -0,0 +1,50 @@
+console.log ("ifstatements")
+// Write an if statement that prints "is greater than" if 5 is greater than 3 
+
+if (5 > 3)
+ {
+ console.log ("is greater than")
+    
+}
+var cat="cat"
+var dog="dog"
+if('cat'.length>=3){
+console.log ("is the length")
+}
+if (cat===dog) {console.log ("the same")
+    
+}else{console.log ("not the same")
+}
+var person = {
+    name: "Bobby",
+    age: 12
+}
+if (person.age>= 18){console.log (person.name+" is allowed to go to the movies.")
+    
+}else{console.log (person.name+" is not allowed to go to the movies.")
+}
+if (person.name.charAt(0)==="B"){console.log (person.name+" is allowed to go to the movies.")
+    1
+
+}else{console.log (person.name+" is not allowed to go to the movies.")}
+if (person.name.charAt(0)==="B"&&person.age>= 18){console.log (person.name+" is allowed to go to the movies.")
+    
+
+}else{console.log (person.name+" is not allowed to go to the movies.")}
+var num = 1
+var string = "2"
+//prints "strict" if 1 strictly equals "1" WHEN
+if (num===string) {console.log ("strict")}
+else if (num==string) {console.log ("loose")}
+else{console.log ("not equal at all")}
+
+// console.log(num===string)
+// console.log(num==string)
+// console.log(1=="1")
+var num1= 1
+var num2= 2
+//Write an if statement that console.logs "yes" if num1 is less than or equal to num2 AND (&&) num2 is equal to 4.
+//<= 
+if (num1<=num2) {console.log ("yes")}
+else if (num2=num4) {console.log ("&&")}
+    
