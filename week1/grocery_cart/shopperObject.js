a68af0289f4edc2b12723a61f1e0e1180d96d0f5
Robin Johnson <57020290+Robin44girl@users.noreply.github.com>
2020-04-10 14:39:09 -0400

    shopperObject.js

--- week1/grocery_cart/shopperObject.js
+++ week1/grocery_cart/shopperObject.js
@@ -1,44 +1,12 @@
-var string ="5"; //string
-// console.log("string"); //string
-// console.log(`string ${string}`); //string
-// console.log(5); //number
-
-// console.log("5"); //string
-
-// console.log(true); //boolean
-// console.log(false); //boolean
-
-var array = [ //arrays and everything in javascript are 0-indexed => eveything starts on 0 not 1
-  ["string", "apple"], //0
-  2,  //1
-  "2", //2
-  {3: "3", another: "another value"}, //{key: value} //3
- [ 4, "4"] //4
-]
-// console.log(array[3]); //array
-//an array can be made up of anything. 
-//it can be an array of numbers 
-// OR an array of strings
-//OR an array of objects
-//OR an array of arrays
-
-// [     ]
-
-// {      }
-
-
 var shopper={
   firstName:"Wegmans",
   // key:value,
   age:20,
   isAlive: true,
-  function: function myFunction() {
+  var groceries=["milk", "bread", "grapes", "cereal", "chips", "juice",]
+  myFunction: function() {
     //function logic goes inside of here
     return this.firstName + " is " + this.age +"years old"
-    var groceries=["milk", "bread", "grapes", "cereal", "chips", "juice",];
-
-  }
-
-
 }
-console.log(shopper)
+{
+  console.log(shopper.myFunction)();}
